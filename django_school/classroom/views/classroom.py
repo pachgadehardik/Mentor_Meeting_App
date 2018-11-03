42c26cad4fd88f8aace83936d11f12b6eb2caea3
unknown <pachgadehardik@gmail.com>
2018-11-03 19:02:17 +0530

    Mentor Meet

---
+++ django_school/classroom/views/classroom.py
@@ -0,0 +1,17 @@
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
+
+
+class SignUpView(TemplateView):
+    template_name = 'registration/signup.html'
+
+
+def home(request):
+    if request.user.is_authenticated:
+        if request.user.is_teacher:
+            return redirect('teachers:quiz_change_list')
+        elif request.user.is_head:
+            return redirect('teachers:headpage')
+        else:
+            return redirect('students:quiz_list')
+    return render(request, 'classroom/home.html')
